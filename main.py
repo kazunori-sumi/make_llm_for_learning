073d97feefeea6c51675c47501d6e2141e275a7c
kazunori-sumi <ksumi1988@icloud.com>
2026-03-13 15:44:39 +0900

    ボキャブラリーに含まれないワードがあるとエラーになるところまで確認

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import urllib.request
+import re
 from simple_tokenizer import SimpleTokenizerV1          
 
 def retrieve_raw_text():
@@ -9,12 +10,20 @@ def retrieve_raw_text():
     file_path = "the-verdict.txt"
     urllib.request.urlretrieve(url, file_path)
 
+def parse_token(raw):
+    preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw)
+    preprocessed = [item.strip() for item in preprocessed if item.strip()]
+    all_words = sorted(set(preprocessed))
+    vocab = {token:integer for integer,token in enumerate(all_words)}
+    return vocab
+
 def main():
     # retrieve_raw_text()
     with open("the-verdict.txt", "r", encoding="utf-8") as f:
         raw_text = f.read()
-        tokenizer = SimpleTokenizerV1(raw_text)
-    
+        vocab = parse_token(raw_text)
+        tokenizer = SimpleTokenizerV1(vocab)
+        print(tokenizer.encode('Hello, do you wanna eat lunch'))
 
 if __name__ == "__main__":
     main()
