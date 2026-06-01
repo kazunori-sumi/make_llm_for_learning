2806e6fb65d23ae1cd5570d421aa07a49e807ddc
kazunori-sumi <ksumi1988@icloud.com>
2026-06-01 17:05:59 +0900

    正規化層を実装

--- dummy_gpt_model.py
+++ dummy_gpt_model.py
@@ -10,7 +10,7 @@ class DummyGPTModel(nn.Module):
     self.trf_blocks = nn.Sequential(
       *[DummyTransformerBlock(cfg) for _ in range(cfg["n_layers"])]
     )
-    self.final_norm = DummyLayerNorm(cfg["emb_dim"])
+    self.final_norm = LayerNorm(cfg["emb_dim"])
     self.out_head = nn.Linear(
       cfg["emb_dim"], cfg["vocab_size"], bias=False
       )
@@ -28,12 +28,18 @@ class DummyGPTModel(nn.Module):
     logits = self.out_head(x)
     return logits
 
-class DummyLayerNorm(nn.Module):
-  def __init__(self, normalized_shape, eps=1e-5):
+class LayerNorm(nn.Module):
+  def __init__(self, emb_dim):
     super().__init__()
+    self.eps = 1e-5
+    self.scale = nn.Parameter(torch.ones(emb_dim))
+    self.shift = nn.Parameter(torch.zeros(emb_dim))
 
   def forward(self, x):
-    return x
+    mean = x.mean(dim=-1, keepdim=True)
+    var = x.var(dim=-1, keepdim=True, unbiased=False)
+    norm_x = (x - mean) / torch.sqrt(var + self.eps)
+    return self.scale * norm_x + self.shift
 
 class DummyTransformerBlock(nn.Module):
   def __init__(self, cfg):
--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import urllib.request
 import re
 import tiktoken
 import torch
+import torch.nn as nn
 
 from dummy_gpt_model import DummyGPTModel
+from dummy_gpt_model import LayerNorm
 
 def main():
     GPT_CONFIG_124M = {
@@ -24,12 +26,16 @@ def main():
     batch.append(torch.tensor(tokenizer.encode(txt1)))
     batch.append(torch.tensor(tokenizer.encode(txt2)))
     batch = torch.stack(batch, dim=0)
-    
-    
+
     torch.manual_seed(123)
     model = DummyGPTModel(GPT_CONFIG_124M)
     logits = model(batch)
     print(logits)
+
+    batch_example = torch.randn(2,5)
+    layer = nn.Sequential(nn.Linear(5,6), nn.ReLU())
+    out = layer(batch_example)
+    print(out)
 
 if __name__ == "__main__":
     main()
